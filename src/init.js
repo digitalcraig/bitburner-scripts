10535c7812df9f4a09b1b1932a5fd80ffad8e31e
Craig Leikis <craig@leikis.org>
2021-12-24 09:44:15 -0600

    Make pulling new scripts and resetting server map options

--- src/init.js
+++ src/init.js
@@ -16,26 +16,44 @@ function localeHHMMSS(ms = 0) {
 }
 
 export async function main(ns) {
-  ns.tprint(`[${localeHHMMSS()}] Downloading scripts`)
+    ns.tprint(`[${localeHHMMSS()}] Downloading scripts`)
 
-  let hostname = ns.getHostname()
+    let hostname = ns.getHostname()
+    let nopull = false
+    let keepservers = false
 
-  if (hostname !== 'home') {
-    throw new Exception('Run the script from home')
-  }
+    if (hostname !== 'home') {
+        throw new Exception('Run the script from home')
+    }
 
-  for (let i = 0; i < filesToDownload.length; i++) {
-    const filename = filesToDownload[i]
-    const path = baseUrl + filename
-    await ns.scriptKill(filename, 'home')
-    await ns.rm(filename)
-    await ns.sleep(200)
-    ns.tprint(`[${localeHHMMSS()}] Trying to download ${path}`)
-    await ns.wget(path + '?ts=' + new Date().getTime(), filename)
-  }
+    if (args.length > 0 ) {
+        for (let i = 0; i < args.length; i++) {
+            if (args[i] == 'nopull') {
+                nopull = true
+            }
+            if (args[i] == 'keepservers') {
+                keepservers = true
+            }
+        }
+    }
 
-  valuesToRemove.map((value) => localStorage.removeItem(value))
+    if (nopull == false) {
+        for (let i = 0; i < filesToDownload.length; i++) {
+                const filename = filesToDownload[i]
+                const path = baseUrl + filename
+                await ns.scriptKill('/'+filename, 'home')
+                await ns.rm('/'+filename)
+                await ns.sleep(200)
+                ns.tprint(`[${localeHHMMSS()}] Trying to download ${path} to /`+filename)
+                await ns.wget(path + '?ts=' + new Date().getTime(), '/'+ filename)
+        }
+    }
 
-  //ns.tprint(`[${localeHHMMSS()}] Spawning killAll.ns`)
- //ns.spawn('killAll.ns', 1, 'runHacking.ns')
+    if (keepservers == false) {
+        ns.tprint(`[${localeHHMMSS()}] Clearing server map`)
+        valuesToRemove.map((value) => localStorage.removeItem(value))
+    }
+
+    //ns.tprint(`[${localeHHMMSS()}] Spawning killAll.ns`)
+    //ns.spawn('killAll.ns', 1, 'runHacking.ns')
 }
