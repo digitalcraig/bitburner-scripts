76b4589848208df77c838a46a3e2f2adefc277b5
Craig Leikis <craig@leikis.org>
2021-12-24 10:20:58 -0600

    Remove submodule and get directly from official scripts

--- src/init.js
+++ src/init.js
@@ -1,10 +1,17 @@
 /** @param {NS} ns **/
 
-const baseUrl = 'https://raw.githubusercontent.com/digitalcraig/bitburner-scripts/master/src/'
-const filesToDownload = [
-  'official/basic_hack.js',
-  'official/analyze_server.js',
+const officialBaseUrl = 'https://raw.githubusercontent.com/bitburner-official/bitburner-scripts/master/'
+const officialFilesToDownload = [
+    'analyze_server.js',
+    'basic_hack.js',
+    'custom-stats.js',
+    'deploy.js'
 ]
+const officialPrefix = '/official'
+
+const baseUrl = 'https://raw.githubusercontent.com/digitalcraig/bitburner-scripts/master/src/'
+const filesToDownload = []
+const prefix = '/'
 
 const valuesToRemove = ['BB_SERVER_MAP']
 
@@ -14,6 +21,18 @@ function localeHHMMSS(ms = 0) {
   }
 
   return new Date(ms).toLocaleTimeString()
+}
+
+function pull(ns, baseUrl, filesToDownload, prefix) {
+    for (let i = 0; i < filesToDownload.length; i++) {
+        const filename = filesToDownload[i]
+        const path = baseUrl + filename
+        await ns.scriptKill('/'+filename, 'home')
+        await ns.rm('/'+filename)
+        await ns.sleep(200)
+        ns.tprint(`[${localeHHMMSS()}] Trying to download ${path} to `,prefix + filename)
+        await ns.wget(path + '?ts=' + new Date().getTime(), prefix + filename)
+    }
 }
 
 export async function main(ns) {
@@ -38,16 +57,8 @@ export async function main(ns) {
     }
 
     if (nopull == false) {
-        ns.tprint(`[${localeHHMMSS()}] Downloading scripts`)
-        for (let i = 0; i < filesToDownload.length; i++) {
-                const filename = filesToDownload[i]
-                const path = baseUrl + filename
-                await ns.scriptKill('/'+filename, 'home')
-                await ns.rm('/'+filename)
-                await ns.sleep(200)
-                ns.tprint(`[${localeHHMMSS()}] Trying to download ${path} to /`+filename)
-                await ns.wget(path + '?ts=' + new Date().getTime(), '/'+ filename)
-        }
+        ns.tprint(`[${localeHHMMSS()}] Downloading official scripts`)
+        pull(officialBaseUrl, officialFilesToDownload, '/official/')
     }
 
     if (keepvalues == false) {
