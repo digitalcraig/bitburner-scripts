deeb8e61cee514b28ef2f04608385db82d65c467
Craig Leikis <craig@leikis.org>
2021-12-26 10:15:08 -0600

    Add second to ms delay in hackscripts

--- src/grow.js
+++ src/grow.js
@@ -11,10 +11,10 @@ export function localeHHMMSS(ms = 0) {
 export async function main(ns) {
     const target = ns.args[0]
     const threads = ns.args[1]
-    const delay = ns.args[2]
+    const delay = ns.args[2] * 1000
   
     if (delay && delay > 0) {
-      await ns.sleep(delay)
+      await ns.sleep(delay*1000)
     }
   
     ns.tprint(`[${localeHHMMSS()}] Starting operation: grow on ${target} in ${threads} threads`)
--- src/weaken.js
+++ src/weaken.js
@@ -11,7 +11,7 @@ export function localeHHMMSS(ms = 0) {
 export async function main(ns) {
     const target = ns.args[0]
     const threads = ns.args[1]
-    const delay = ns.args[2]
+    const delay = ns.args[2] * 1000
   
     if (delay && delay > 0) {
       await ns.sleep(delay)
